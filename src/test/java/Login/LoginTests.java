88c367adcd8ff01567b12a05bb2d15db224bb16e
emrikygithub <emriky@gmail.com>
2020-03-28 17:21:35 +0100

    push first code commit

---
+++ src/test/java/Login/LoginTests.java
@@ -0,0 +1,34 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+    private WebDriver driver;
+
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+
+        Thread.sleep(5000);
+
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("email")).sendKeys("emriky@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("boxedup3002");
+        driver.findElement(By.id("loginbutton")).click();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        LoginTests test = new LoginTests();
+        test.setUp();
+
+    }
+}
